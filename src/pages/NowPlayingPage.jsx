a5de4ed90ee46550155a4056d6c293ea7aa6dcb6
codmoni <160315926+codmoni@users.noreply.github.com>
2024-05-21 16:29:17 +0900

    Update NowPlayingPage.jsx

--- src/pages/NowPlayingPage.jsx
+++ src/pages/NowPlayingPage.jsx
@@ -32,7 +32,7 @@ const NowPlayingPage= () => {
                     method: 'GET',
                     headers: {
                       accept: 'application/json',
-                      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4ZmE5ZDI5YzRhNWQ4OGJhY2QxODIxOWVhZmZlYzZlMCIsInN1YiI6IjY2MmU1NzMyMDNiZjg0MDEyNWVhZGE5MSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.w00sWoRJw_0sqWhMuuINho69qGOHu_jkC9HbH2Rw2Hk'
+                      Authorization: 'Bearer API_KEY'
                     }
                 }
                 const response = await fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1', options);
